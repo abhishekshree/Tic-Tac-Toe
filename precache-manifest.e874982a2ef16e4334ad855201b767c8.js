70429c0d94c937f76fe8c7a259991c6ee44aa0f0
Abhishek Shree <abhi1234456@gmail.com>
2020-04-23 21:57:07 +0530

    Updates

---
+++ precache-manifest.e874982a2ef16e4334ad855201b767c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23e137f6ad6b6076566509573d716e7",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "d8aa2cecd91c75850a4b",
+    "url": "/Tic-Tac-Toe/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "38c58a404a3fb46dd5f4",
+    "url": "/Tic-Tac-Toe/static/js/2.b102e267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.b102e267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8aa2cecd91c75850a4b",
+    "url": "/Tic-Tac-Toe/static/js/main.ffdf7acd.chunk.js"
+  },
+  {
+    "revision": "fc0729295df9e52cea5c",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.836ad489.js"
+  }
+]);
